c5b85cd867c759f2e00fed8351cafe13b59065c8
Maksym Havryliuk <maks.havryliuk@gmail.com>
2017-10-18 01:03:14 +0300

    Начал работу над заданием 4 по теме Классы и объекты

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassesAndObjectsTask4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Money purse = new Money(50,10);
+            Console.WriteLine(purse);
+            Console.WriteLine(purse.CheckAvailability(251));       // Указываем стоимость ед. товара
+            Console.WriteLine(purse.CalculationOfQuantity(251));   // Указываем стоимость ед. товара
+
+
+            Console.ReadKey();
+        }
+    }
+}
